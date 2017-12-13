91218ee4b723a3bc59d2092c601f2ccd3e62340c
Bettina Junghahn <bettinajunghahn@Bettinas-MacBook-Pro.local>
2017-12-13 18:17:21 -0500

    push working final code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,96 +1,83 @@
 
 // Assign a letter to the Computer's guess.
-
-	var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var selection = Math.floor(Math.random() * choices.length);
-	var computerChoice = choices[selection];
+		var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+		var computerChoice =  choices[Math.floor(Math.random() * choices.length -1)];
 		console.log(computerChoice);	
 	 
 
 //Create values that will track out wins, losses, guesses left, letters guessed.
-	
-	// var userText = document.getElementById("user-text");
 	var wins = 0;
 	var losses = 0;
 	var guessesLeft = 9;
 	var lettersGuessed = [];
 	
 
+//Function to reset the game
+			function resetGame() {
+				guessesLeft = 9;
+				lettersGuessed = [];
+				computerChoice = choices[Math.floor(Math.random() * choices.length -1)];
+				console.log(computerChoice);
+			}
 
+
+// Game function.
 // Have user type in a letter and store that value.
-	
-		document.onkeyup = function(event) {
-			var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-			// console.log(userGuess);
-
+	document.onkeyup = function(event) {
+		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 			lettersGuessed.push(" " + userGuess);
-			// console.log(lettersGuessed);
+			
 
-		do {	
-			// Determine whether user's choice matches computer's choice.
-			// If they match, user wins. Increase winning score by 1; reset computer guess and number of guesses.
+		// Determine whether user's choice matches computer's choice.
+		// If they match, user wins. Increase winning score by 1; reset computer guess and number of guesses.
 			if (userGuess == computerChoice) {
 				wins ++;
+				resetGame();
+				
 			// If it does not match, add letter to User guesses and allow another guess from user. (9 in total).
-			} else {
+			} else if (userGuess != computerChoice && guessesLeft != 1) {
 				guessesLeft --;
-			
 			}
 
-			if (guessesLeft < 1) {
+			// If user doesn't guess correctly after 9 tries, record a loss.
+			else {
 				losses ++;
+				resetGame();
 			}
-			// console.log(guessesLeft);
-
-		
-
+			
+		// Display results.
 			var resultDOM = document.getElementById('result');
-
 			var resultHTML = "Wins: " + wins + "<br>" +
-							"Losses: " + losses + "<br>" +
-							"Guesses left: " + guessesLeft + "<br>" +
-							"Letters guessed: " + lettersGuessed;
+								"Losses: " + losses + "<br>" +
+								"Guesses left: " + guessesLeft + "<br>" +
+								"Letters guessed: " + lettersGuessed;
 
-			resultDOM.innerHTML = resultHTML;
-
-		} while (guessesLeft != 0) ;
-
+			resultDOM.innerHTML = resultHTML;	
 }
 
 	
+ 
+			
+
+	
 
 
 
 
 
 
-	// -------
-
-	// 	// Determine whether user's choice matches computer's choice.	
-	// 	// If they match, user wins. Increase winning score by 1; reset computer guess and number of guesses.
-	// 	if(computerChoice == userGuess) {
-	// 		wins++;
-	// 	}
-
-
-	// 	// If it does not match, add letter to User guesses and allow another guess from user. (9 in total).
-	// 		else if(computerChoice != userGuess)  
-	// 			guessesSoFar--;
-
-	// 	}
-
-
-	// 	// If user guess doesn't match computer choice after 9 guesses, add to user losses score.
-	// 		else if (guessesLeft < 1) {
-	// 			losses++;
-	// 		}
-
-
-
-	// 	// After a win or a loss, restart the game (guesses left and user guesses), keeping the score as is.
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+	
